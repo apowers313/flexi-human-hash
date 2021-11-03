9ffc9e22f407f3171187422600a592bf0689ac47
Adam Powers <apowers@ato.ms>
2021-11-02 20:04:05 -0700

    improve random, add input parsing

--- test/FlexiHumanHash.js
+++ test/FlexiHumanHash.js
@@ -4,11 +4,13 @@ const {FlexiHumanHash, FlexiArrayDict} = require("..");
 const randomBuf = new ArrayBuffer(10);
 const tmpUint8 = new Uint8Array(randomBuf);
 // our test array is 8 bits, this is 1, 2, 3, 4, ... in 3 bit numbers:
-tmpUint8.set([0xA0, 0x9C, 0xEE, 0xA0, 0x9C, 0xEE, 0xA0, 0x9C, 0xEE]);
+// tmpUint8.set([0xA0, 0x9C, 0xEE, 0xA0, 0x9C, 0xEE, 0xA0, 0x9C, 0xEE]);
+tmpUint8.set([0b00000101, 0b00111001, 0b01110111, 0b00000101, 0b00111001, 0b01110111, 0b00000101, 0b00111001, 0b01110111]);
 
+// dict length is a power of 2 to prevent random overflow selection
+let testDict = ["frog", "bear", "tree", "cat", "wolf", "octopus", "tiger", "slug"];
 function testDictCreate(opts) {
-    // dict length is a power of 2 to prevent random overflow selection
-    return new FlexiArrayDict("_test", ["frog", "bear", "tree", "cat", "wolf", "octopus", "tiger", "slug"], opts);
+    return new FlexiArrayDict("_test", testDict, opts);
 }
 
 describe("FlexiHumanHash", function() {
@@ -21,49 +23,153 @@ describe("FlexiHumanHash", function() {
     });
 
     it("is constructable", function() {
-        let fhh = new FlexiHumanHash("{{noun}}");
+        let fhh = new FlexiHumanHash("{{test}}");
         assert.isObject(fhh);
     });
 
     describe("format", function() {
         it("accepts args", function() {
-            new FlexiHumanHash("{{noun}}");
+            new FlexiHumanHash("{{test}}");
         });
 
         it("accepts same dictionaries with different args", function() {
-            let fhh = new FlexiHumanHash("{{noun uppercase}}-{{noun caps}}");
+            let fhh = new FlexiHumanHash("{{test uppercase}}-{{test caps}}");
             let str = fhh.hash(randomBuf);
-            console.log("str", str);
+            assert.strictEqual(str, "Frog-BEAR");
         });
     });
 
-    describe.only("hash", function() {
-        it("doesn't require a random number");
+    describe("hash", function() {
+        it("doesn't require a random number", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+            let str = fhh.hash();
+            assert.isTrue(testDict.includes(str));
+        });
+
+        it("eventually randomly selects everything in the list", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+            let coverageSet = new Set();
+            while (coverageSet.size !== testDict.length) {
+                let str = fhh.hash();
+                coverageSet.add(str);
+            }
+        });
+
+        it("overflow eventually randomly selects everything in the list", function() {
+            let overflowDict = ["frog", "bear", "tree", "cat", "wolf", "octopus", "tiger", "slug", "ogre", "burp", "blarg", "foo", "bar", "baz", "bat", "binkey"];
+            function overflowDictCreate(opts) {
+                return new FlexiArrayDict("_overflow", overflowDict, opts);
+            }
+            FlexiHumanHash.registerDictionary("overflow", overflowDictCreate);
+
+            let fhh = new FlexiHumanHash("{{overflow}}");
+            assert.strictEqual(fhh.entropy, BigInt(16));
+
+            let coverageSet = new Set();
+            while (coverageSet.size !== overflowDict.length) {
+                let str = fhh.hash();
+                // console.log("coverageSet", coverageSet.size);
+                coverageSet.add(str);
+            }
+        });
+
         it("accepts string", function() {
             let fhh = new FlexiHumanHash("{{test}}");
-            fhh.hash("foo");
-            throw Error("not implemented");
+            let str = fhh.hash("}");
+            assert.strictEqual(str, "cat");
+            str = fhh.hash("^");
+            assert.strictEqual(str, "tree");
+            str = fhh.hash("0");
+            assert.strictEqual(str, "bear");
+            str = fhh.hash("\t");
+            assert.strictEqual(str, "frog");
         });
 
         it("accepts ArrayBuffer", function() {
             let fhh = new FlexiHumanHash("{{test}}");
-            let ret = fhh.hash(randomBuf);
-            console.log("ret", ret);
+            let str = fhh.hash(randomBuf);
+            assert.strictEqual(str, "frog");
         });
 
-        it("accepts TypedArray");
-        it("accepts array of numbers");
-        it("accepts iterable of numbers");
-        it("does md5");
-        it("does sha256");
-        it("salts hash");
+        it("accepts TypedArray", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
 
-        // it("accepts array of hex strings");
-        // it("accepts array of hex strings with leading 0x");
-        // it("accepts hex string");
-        // it("accepts hex string with spaces");
-        // it("accepts hex string with leading 0x");
-        // it("accepts hex string with leading 0x and spaces");
+            // Uint8Array
+            let str = fhh.hash(new Uint8Array([0b00000000]));
+            assert.strictEqual(str, "frog");
+            str = fhh.hash(new Uint8Array([0b00100000]));
+            assert.strictEqual(str, "bear");
+            str = fhh.hash(new Uint8Array([0b01000000]));
+            assert.strictEqual(str, "tree");
+            str = fhh.hash(new Uint8Array([0b01100000]));
+            assert.strictEqual(str, "cat");
+            str = fhh.hash(new Uint8Array([0b10000000]));
+            assert.strictEqual(str, "wolf");
+            str = fhh.hash(new Uint8Array([0b10100000]));
+            assert.strictEqual(str, "octopus");
+            str = fhh.hash(new Uint8Array([0b11000000]));
+            assert.strictEqual(str, "tiger");
+            str = fhh.hash(new Uint8Array([0b11100000]));
+            assert.strictEqual(str, "slug");
+
+            // Uint16Array
+            str = fhh.hash(new Uint8Array([0b1010000010100000])); // endianness :(
+            assert.strictEqual(str, "octopus");
+        });
+
+        it("accepts array of numbers", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+
+            // array of numbers
+            let str = fhh.hash([0b00000000, 1, 2, 3, 4]);
+            assert.strictEqual(str, "frog");
+            str = fhh.hash([0b00100000]);
+            assert.strictEqual(str, "bear");
+            str = fhh.hash([0b01000000]);
+            assert.strictEqual(str, "tree");
+            str = fhh.hash([0b01100000]);
+            assert.strictEqual(str, "cat");
+            str = fhh.hash([0b10000000]);
+            assert.strictEqual(str, "wolf");
+            str = fhh.hash([0b10100000]);
+            assert.strictEqual(str, "octopus");
+            str = fhh.hash([0b11000000]);
+            assert.strictEqual(str, "tiger");
+            str = fhh.hash([0b11100000]);
+            assert.strictEqual(str, "slug");
+
+            fhh = new FlexiHumanHash("{{test}}:{{test}}:{{test}}:{{test}}:{{test}}:{{test}}");
+            str = fhh.hash([0b11100000, 0b11011001, 0b10000000]);
+            assert.strictEqual(str, "slug:frog:bear:octopus:wolf:tiger");
+        });
+
+        it("accepts iterable of numbers", function() {
+            fhh = new FlexiHumanHash("{{test}}:{{test}}:{{test}}:{{test}}:{{test}}:{{test}}");
+            let s = new Set([0b11100000, 0b11011001, 0b10000000]);
+            str = fhh.hash(s.values());
+            assert.strictEqual(str, "slug:frog:bear:octopus:wolf:tiger");
+        });
+
+        it("does md5", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+            let str = fhh.hash("this is a very long string that is more than we need", {hashAlg: "md5"});
+            assert.strictEqual(str, "slug");
+        });
+
+        it("does sha256", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+            let str = fhh.hash("this is a very long string that is more than we need", {hashAlg: "sha256"});
+            assert.strictEqual(str, "frog");
+        });
+
+        it("salts hash", function() {
+            let fhh = new FlexiHumanHash("{{test}}");
+            let str = fhh.hash("this is a very long string that is more than we need", {hashAlg: "sha256"});
+            assert.strictEqual(str, "frog");
+
+            str = fhh.hash("this is a very long string that is more than we need", {hashAlg: "sha256", hashSalt: "foo"});
+            assert.strictEqual(str, "cat");
+        });
     });
 
     describe("transform", function() {
@@ -99,7 +205,7 @@ describe("FlexiHumanHash", function() {
             assert.strictEqual(str, "cat");
         });
 
-        it.only("exact-length", function() {
+        it("exact-length", function() {
             let fhh = new FlexiHumanHash("{{test exact-length=5}}");
             assert.strictEqual(fhh.entropy, BigInt(1));
             let str = fhh.hash(randomBuf);
